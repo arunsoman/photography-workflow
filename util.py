9ebdc82910743c6fd8651fb91746bf1ec009aec7
Arun Soman <aarunsoman@gmail.com>
2018-05-07 16:39:33 +0530

    Update util.py

--- util.py
+++ util.py
@@ -1,39 +1,39 @@
 import os
+import shutil
+import time
 import platform
-from datetime import datetime
 
-home = '/'
-
-def scan_cr2(base):
-    for root, dirs, files in os.walk(base):
-        for file in files:
-            if file.upper().endswith(".CR2"):
-                yield os.path.join(root, file)
-
-def get_file_create_date(src):
-    yield time.strftime('%m/%d/%Y', _get_file_created_date_long(src))
-
-def _get_file_created_date_long(src):
-    if platform.system() == 'Windows':
-        yield os.path.getctime(path_to_file)
-    else :
-      stat = os.stat(path_to_file)
-    try:
-        yield stat.st_birthtime 
-    except AttributeError: 
-        yield stat.st_mtime
+home = "c:\\test"
 
 def prepare_destination(name):
     directory = os.path.join(home, name)
     if not os.path.exists(directory):
         os.makedirs(directory) 
-        yield directory
+        return directory
+    
+def transfer(files):
+  for aFile, createdDate in files: 
+    des = prepare_destination(createdDate)
+    shutil.move(aFile, des)
+    
+def get_file_create_date(src):
+    return time.strftime('%m/%d/%Y', _get_file_created_date_long(src))
 
-def transfer(file):
-    date = get_file_create_date(file) 
-    des = prepare_destination(date)
-    shutil.move(src, des)
-
-def train():
-    sd_loc = ''
-    for a_cr2 in scan_cr2(src) transfer_file(a_cr2)
+def _get_file_created_date_long(src):
+    if platform.system() == 'Windows':
+        return os.path.getctime(src)
+    else:
+      stat = os.stat(src)
+    try:
+        return stat.st_birthtime 
+    except AttributeError: 
+        return stat.st_mtime
+        
+def scan_cr2(base):
+    for root, dirs, files in os.walk(base):
+        for file in files:
+            if file.upper().endswith(".CR2"):
+                yield os.path.join(root, file), get_file_create_date(file)
+                
+def test():
+  transfer(scan_cr2("c:\\tmp"))
