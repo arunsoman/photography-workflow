61f9ca84e998c8b07ffe1b4bccd97e2355b41168
Arun Soman <aarunsoman@gmail.com>
2018-05-09 12:54:27 +0530

    Update util.py

--- util.py
+++ util.py
@@ -1,39 +1,45 @@
 import os
 import shutil
-import time
+import datetime
 import platform
 
 home = "c:\\test"
 
+
 def prepare_destination(name):
     directory = os.path.join(home, name)
     if not os.path.exists(directory):
-        os.makedirs(directory) 
+        os.makedirs(directory)
         return directory
-    
-def transfer(files):
-  for aFile, createdDate in files: 
-    des = prepare_destination(createdDate)
-    shutil.move(aFile, des)
-    
-def get_file_create_date(src):
-    return time.strftime('%m/%d/%Y', _get_file_created_date_long(src))
+
+
+def transfer_and_del(files):
+    for aFile, createdDate in files:
+        des = prepare_destination(createdDate)
+        shutil.move(aFile, des)
+        os.remove(aFile)
 
 def _get_file_created_date_long(src):
     if platform.system() == 'Windows':
         return os.path.getctime(src)
     else:
-      stat = os.stat(src)
+        stat = os.stat(src)
     try:
-        return stat.st_birthtime 
-    except AttributeError: 
+        return stat.st_birthtime
+    except AttributeError:
         return stat.st_mtime
-        
+
+
+def get_file_create_date(src):
+    return datetime.datetime.fromtimestamp(_get_file_created_date_long(src)).strftime('%d-%m-%y')
+
+
 def scan_cr2(base):
     for root, dirs, files in os.walk(base):
         for file in files:
             if file.upper().endswith(".CR2"):
                 yield os.path.join(root, file), get_file_create_date(file)
-                
+
+
 def test():
-  transfer(scan_cr2("c:\\tmp"))
+    transfer_and_del(scan_cr2("c:\\tmp"))
